8cc767c477cad67f0e96657b5161a0540c87ff32
gneves <gneves@mmcafe.com.br>
2018-09-03 19:19:41 -0300

    classe departamentos

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -7,10 +7,11 @@ class ProdutosController < ApplicationController
 
     def new 
         @produto = Produto.new 
+        @departamentos = Departamento.all
     end
 
     def create
-        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
+        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :Departamento_id
         @produto = Produto.new valores
         puts "*"*100
         puts @produto
@@ -19,6 +20,7 @@ class ProdutosController < ApplicationController
             flash[:notice] = "Produto salvo com sucesso"
             redirect_to root_url
         else
+            @departamentos = Departamento.all
             render :new    
         end
     end
@@ -33,6 +35,26 @@ class ProdutosController < ApplicationController
         @nome_a_buscar = params[:nome]
         @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
 
+    end
+
+    def edit
+        id = params[:id]
+        @produto = Ṕroduto.find(id)
+        @departamentos = Departamento.all
+        render :new
+    end
+
+    def update
+        id = params[:id]
+        @produto = Produto.find(id)
+        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :Departamento_id
+        if @produto.update valores
+            flash[:notice] = "Produto salvo com sucesso"
+            redirect_to root_url
+        else
+            @departamentos = Departamento.all
+            render :new
+        end
     end
 
 end
